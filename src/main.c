ca76b4ddc68044213e3d3df301f2ed062e6c8331
Hao-Zhong <88621562+Hao20210808@users.noreply.github.com>
2023-11-24 12:00:48 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "huffman.c"
 
-// Driver code
+#define MAX_SIZE 100
+
 int main()
 {
+    char arr[MAX_SIZE][MAX_SIZE];
+    int freq[MAX_SIZE];
 
-    char arr[] = {'a', 'b', 'c', 'd', 'e'};
-    int freq[] = {20, 23, 11, 24, 9};
+    int i = 0;
+    while (i < MAX_SIZE)
+    {
+        printf("%d:", i+1);
+        printf("Data was built SUCCESSFULLY! \nPlease enter text to represent a data: ");
+        scanf("%s", arr[i]);
+        printf("Please enter number to input a data: ");
+        scanf("%d", &freq[i]);
+        printf("\n");
 
-    int size = sizeof(arr) / sizeof(arr[0]);
+        char scanner;
+        int check;
+        printf("If you want to build another data, then continue (Y/n): ");
+        scanf(" %c", &scanner);
 
-    HuffmanCodes(arr, freq, size);
+        if (scanner != 'Y' && scanner != 'y'){
+            break;
+        }
+        i++;
+        printf("--------------------------------------------------------\n");
+    }
 
+    int size = i + 1;
+
+    HuffmanCodes((char **)arr, freq, size);
     return 0;
 }
